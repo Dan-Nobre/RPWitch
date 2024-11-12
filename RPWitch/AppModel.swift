8d83460bef1c3fb3500fd6c5fbbd0e91e3c5cd41
Daniel Nobre <55868687+Dan-Nobre@users.noreply.github.com>
2024-11-12 13:57:12 -0300

    Initial Commit

---
+++ RPWitch/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  RPWitch
+//
+//  Created by Daniel Nobre on 12/11/24.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ RPWitch/ImmersiveView.swift
@@ -0,0 +1,30 @@
+//
+//  ImmersiveView.swift
+//  RPWitch
+//
+//  Created by Daniel Nobre on 12/11/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ImmersiveView: View {
+
+    var body: some View {
+        RealityView { content in
+            // Add the initial RealityKit content
+            if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+                content.add(immersiveContentEntity)
+
+                // Put skybox here.  See example in World project available at
+                // https://developer.apple.com/
+            }
+        }
+    }
+}
+
+#Preview(immersionStyle: .progressive) {
+    ImmersiveView()
+        .environment(AppModel())
+}
